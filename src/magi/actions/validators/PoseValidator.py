768c577f34fc0a4cf04fd6e3f7d5b165dc053703
Shushman <shushmanchoudhury@gmail.com>
2016-12-22 18:41:39 +0530

    PoseValidator fixed except for docstrings and import statements

--- src/magi/actions/validators/PoseValidator.py
+++ src/magi/actions/validators/PoseValidator.py
@@ -3,13 +3,13 @@ from ..validate import Validator
 from ..base import to_key, from_key, ValidationError
 
 import logging
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 class ObjectPoseValidator(Validator):
     def __init__(self, obj_name, pose, name='PoseValidator'):
         """
-        This class validates that object is in pose 
+        This class validates that object is in pose
         """
         super(ObjectPoseValidator, self).__init__(name=name)
         self.obj_name = obj_name
@@ -17,19 +17,19 @@ class ObjectPoseValidator(Validator):
 
     def validate(self, env, detector=None):
         """
-        @throws An ValidationError if pose is not close enough 
+        @throws An ValidationError if pose is not close enough
         """
         from table_clearing.perception_utils import get_obj, PerceptionException
 
         if detector:
-            logger.info('Redetect before validation')
+            LOGGER.info('Redetect before validation')
             detector.Update(env.GetRobots()[0])
 
         with env:
             obj = get_obj(env, self.obj_name, first_match=True)
             obj_transform = obj.GetTransform()
             import numpy as np
-            # TODO: how close should this be? 
+            # TODO: how close should this be?
             if not np.allclose(obj_transform, self.expected_pose):
                 raise ValidationError(
                     message='%s not in expected pose' % self.obj_name,
