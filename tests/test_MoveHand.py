f84a32e0bd58d9c71f624cbe0ecb5d16d32bb246
Brian Hou <brian.hou@live.com>
2016-12-24 09:07:54 -0800

    Clean up imports, docstrings for test_MoveHand.

--- tests/test_MoveHand.py
+++ tests/test_MoveHand.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
-import os.path, sys
+
+"""Tests for magi.actions.MoveHand."""
+
+import os.path
+import sys
+import unittest
+
+from prpy.base.endeffector import EndEffector
+import prpy
+
+from magi.actions.MoveHand import MoveHandAction
+from magi_test_helper import MAGITest
+
+# Is this path manipulation necessary? The current directory should already be on the path.
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
-import unittest
-from magi_test_helper import MAGITest
-from prpy.base.endeffector import EndEffector
 
 class DummyHand(EndEffector):
+    """Dummy EndEffector that jumps directly to the given DOF values."""
+
     def __init__(self, manipulator):
         EndEffector.__init__(self, manipulator)
         self.dofs = [0.0]
 
     def move_hand(self, *dofs):
+        """Jump directly to DOFs."""
         dof_values = self.GetDOFValues()
         for idx, dofval in enumerate(dofs):
             if dofval is not None:
@@ -20,13 +33,13 @@ class DummyHand(EndEffector):
         self.SetDOFValues(dof_values)
 
 class TestHandMethods(MAGITest):
+    """Test hand methods."""
 
     def test_MoveHand(self):
-        import prpy
+        """Test MoveHandAction."""
         self.robot.arm = self.robot.GetManipulator('arm')
         self.robot.hand = self.robot.arm.GetEndEffector()
         prpy.bind_subclass(self.robot.hand, DummyHand, manipulator=self.robot.arm)
-        from magi.actions.move_hand import MoveHandAction
         action = MoveHandAction(self.robot.hand, [0.7],
                                 self.robot.hand.GetIndices()[:1])
         return self._action_helper(action)
