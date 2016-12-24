047b69edd1c4894c3248e422c5ebadcfe443a2aa
Brian Hou <brian.hou@live.com>
2016-12-23 20:36:14 -0800

    Fix no-name-in-module error.

--- src/magi/actions/__init__.py
+++ src/magi/actions/__init__.py
@@ -1,5 +1,5 @@
 from magi.actions.Disable import DisableAction
-from magi.actions.MoveHand import MoveHandAction, CloseHandAction, OpenHandAction, GrabObjectAction, ReleaseObjectAction
+from magi.actions.MoveHand import MoveHandAction, GrabObjectAction, ReleaseObjectAction, close_hand_action, open_hand_action
 from magi.actions.MoveUntilTouch import MoveUntilTouchAction
 from magi.actions.PlaceObject import PlaceObjectAction
 from magi.actions.Plan import PlanAction, PlanToTSRAction
@@ -7,15 +7,15 @@ from magi.actions.PushObject import PushObjectAction
 from magi.actions.Sequence import SequenceAction
 
 __all__ = [
-    'CloseHandAction',
     'DisableAction',
     'GrabObjectAction',
     'MoveHandAction',
     'MoveUntilTouchAction',
-    'OpenHandAction',
     'PlanAction',
     'PlanToTSRAction',
     'PushObjectAction',
     'ReleaseObjectAction',
     'SequenceAction',
+    'close_hand_action',
+    'open_hand_action',
 ]
