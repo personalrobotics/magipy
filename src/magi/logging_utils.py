55cd73e18dafa66a23c8d62e44c1ef2390af0487
Brian Hou <brian.hou@live.com>
2016-12-27 00:25:19 -0800

    Document logging_utils.py.

--- src/magi/logging_utils.py
+++ src/magi/logging_utils.py
@@ -1,3 +1,5 @@
+"""MAGI logging utility functions."""
+
 from datetime import datetime
 import logging
 
@@ -5,12 +7,21 @@ from prpy.planning.base import Tags
 from prpy.util import GetTrajectoryTags
 
 def get_logger():
+    """
+    Return the metrics logger.
+
+    @return metrics logger
+    """
     metrics_logger = logging.getLogger('planning_metrics')
     return metrics_logger
 
 
 def setup_logger():
-    # Setup metrics logging
+    """
+    Configure metrics logger.
+
+    @return metrics logger
+    """
     logfile = datetime.now().strftime('trial_%Y%m%d_%H%M.log')
     metrics_logger = get_logger()
     hdlr = logging.FileHandler('%s' % logfile)
@@ -25,7 +36,13 @@ def setup_logger():
 
 def _log_data(path, action_name, header, tag, log_metadata=False):
     """
-    Log data about a path or trajectory
+    Log data about a path or trajectory.
+
+    @param path: trajectory after postprocessing
+    @param action_name: name of Action that generated the trajectory
+    @param header: one-letter header for logs
+    @param tag: tag to filter trajectory tags with
+    @param log_metadata: True if metadata should be logged
     """
     logger = get_logger()
 
@@ -41,26 +58,29 @@ def _log_data(path, action_name, header, tag, log_metadata=False):
 
 def log_plan_data(path, action_name):
     """
-    Log timing and metadata about planning of a path or trajectory
-    @param path The trajectory after postprocessing
-    @param action_name The HGPC action that generated the trajectory
+    Log timing and metadata about planning of a path or trajectory.
+
+    @param path: trajectory after postprocessing
+    @param action_name: name of Action that generated the trajectory
     """
     _log_data(path, action_name, 'P', Tags.PLAN_TIME, log_metadata=True)
 
 
 def log_postprocess_data(traj, action_name):
     """
-    Log timing and metadata about postprocessing of a path or trajectory
-    @param traj The trajectory after postprocessing
-    @param action_name The HGPC action that generated the trajectory
+    Log timing and metadata about postprocessing of a path or trajectory.
+
+    @param traj: trajectory after postprocessing
+    @param action_name: name of Action that generated the trajectory
     """
     _log_data(traj, action_name, 'S', Tags.POSTPROCESS_TIME, log_metadata=True)
 
 
 def log_execution_data(traj, action_name):
     """
-    Log timing data about execution of a trajectory or path
-    @param traj The trajectory to log
-    @param action_name The HGPC action that generated the trajectory
+    Log timing data about execution of a trajectory or path.
+
+    @param traj: trajectory to log
+    @param action_name: name of Action that generated the trajectory
     """
     _log_data(traj, action_name, 'E', Tags.EXECUTION_TIME)
