d3251ae746e2f5b0e87986960cbce2cd0914934f
Brian Hou <brian.hou@live.com>
2016-12-26 22:03:56 -0800

    Document PlaceObject.py.

--- src/magi/actions/PlaceObject.py
+++ src/magi/actions/PlaceObject.py
@@ -1,8 +1,15 @@
+"""Defines the PlaceObjectAction."""
+
 from magi.actions.Plan import PlanAction
 from magi.actions.base import from_key, to_key
 
 
 class PlaceObjectAction(PlanAction):
+    """
+    An Action that places an object onto another object by using the 'point_on'
+    and 'place' TSRs.
+    """
+
     def __init__(self,
                  robot,
                  obj,
@@ -16,17 +23,20 @@ class PlaceObjectAction(PlanAction):
                  planner=None,
                  name=None):
         """
-        Places an object onto another object by using the 'point_on' and 'place' tsrs
-        @param robot The robot
-        @param obj The object to place
-        @param on_obj The object 'obj' should be place on
-        @param active_indices The indices of the robot that should be active during planning
-        @param active_manipulator The manipulator of the robot that should be active during planning
-        @param height The height relative to the on_obj to place the object (meters)
-        @param allowed_tilt The amount of allowed tilt in the object when placing it (radians)
-        @param args Extra arguments to pass to the planner
-        @param kwargs Extra keyword arguments to pass to the planner
-        @param planner A specific planner to use - if None, robot.planner is used
+        @param robot: robot
+        @param obj: object to place
+        @param on_obj: object 'obj' should be placed on
+        @param active_indices: indices of the robot that should be active during
+          planning
+        @param active_manipulator: manipulator of the robot that should be
+          active during planning
+        @param height: height relative to on_obj to place the object (meters)
+        @param allowed_tilt: amount of allowed tilt in the object when placing
+          it (radians)
+        @param args: extra arguments to pass to the planner
+        @param kwargs: extra keyword arguments to pass to the planner
+        @param planner: specific planner to use
+          if None, robot.planner is used
         """
         super(PlaceObjectAction, self).__init__(
             robot,
@@ -45,15 +55,29 @@ class PlaceObjectAction(PlanAction):
         self.allowed_tilt = allowed_tilt
 
     def get_obj(self, env):
+        """
+        Look up and return the object to place in the environment.
+
+        @param env: OpenRAVE environment
+        @return KinBody to place
+        """
         return from_key(env, self._obj)
 
     def get_on_obj(self, env):
+        """
+        Look up and return the object to place on in the environment.
+
+        @param env: OpenRAVE environment
+        @return KinBody to place on
+        """
         return from_key(env, self._on_obj)
 
     def plan(self, env):
         """
-        Generate a tsr for placing the object on another object
-        @param env The OpenRAVE environment
+        Generate a TSR for placing an object on another object.
+
+        @param env: OpenRAVE environment
+        @return a PlanSolution
         """
         obj = self.get_obj(env)
         on_obj = self.get_on_obj(env)
