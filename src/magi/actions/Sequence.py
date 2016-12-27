cd922a87d1d9926ce1ac1dc73dc90c6cd85cea72
Brian Hou <brian.hou@live.com>
2016-12-26 23:13:31 -0800

    Document Sequence.py.

--- src/magi/actions/Sequence.py
+++ src/magi/actions/Sequence.py
@@ -1,3 +1,7 @@
+"""
+Define SequenceAction, SequenceSolution, SequenceExecutableSolution.
+"""
+
 from contextlib import nested
 import logging
 
@@ -8,19 +12,24 @@ LOGGER = logging.getLogger(__name__)
 
 
 class SequenceExecutableSolution(ExecutableSolution):
+    """An ExecutableSolution that performs a sequence of actions in order."""
+
     def __init__(self, solution, executable_solutions):
         """
-        @param solution The solution that generated this ExecutableSolution
-        @param executable_solutions A list of ExecutableSolutions to be executed in order
+        @param solution: Solution that generated this ExecutableSolution
+        @param executable_solutions: list of ExecutableSolutions to be executed
+          in order
         """
         super(SequenceExecutableSolution, self).__init__(solution)
         self.executable_solutions = executable_solutions
 
     def execute(self, env, simulate):
         """
-        Execute all ExecutableSolutions used to initialize this object in order
-        @param env The OpenRAVE environment
-        @param simulate If True, simulate execution
+        Execute all ExecutableSolutions in order.
+
+        @param env: OpenRAVE environment
+        @param simulate: flag to run in simulation
+        @return list of results of executing all solutions
         """
         return [
             executable_solution.execute(env, simulate)
@@ -29,14 +38,18 @@ class SequenceExecutableSolution(ExecutableSolution):
 
 
 class SequenceSolution(Solution):
+    """A Solution that performs a sequence of actions in order."""
+
     def __init__(self,
                  action,
                  solutions,
                  precondition=None,
                  postcondition=None):
         """
-        @param action The Action that generated this solution
-        @param solutions A list of solutions to be processed in order
+        @param action: Action that generated this Solution
+        @param solutions: list of solutions to be processed in order
+        @param precondition: Validator that validates preconditions
+        @param postcondition: Validator that validates postconditions
         """
         deterministic = all(s.deterministic for s in solutions)
         super(SequenceSolution, self).__init__(action, deterministic,
@@ -45,22 +58,26 @@ class SequenceSolution(Solution):
 
     def save_and_jump(self, env):
         """
-        @param env The OpenRAVe environment
-        @return A set of context managers, one for each solution, that must be applied in order
+        Return a sequence of context managers.
+
+        @param env: OpenRAVE environment
+        @return a sequence of context managers, one for each solution, that must
+          be applied in order
         """
         return nested(
             *[solution.save_and_jump(env) for solution in self.solutions])
 
     def save(self, env):
         """
-        Not implemented. Use save_and_jump
+        Not implemented. Use save_and_jump.
         """
         raise ExecutionError('save is unsupported for SequenceSolution')
 
     def jump(self, env):
         """
-        Jump through the solutions, one at a time, in order
-        @param env The OpenRAVe enviornment
+        Jump through the solutions, one at a time, in order.
+
+        @param env: OpenRAVE environment
         """
         for solution in self.solutions:
             solution.jump(env)
@@ -69,14 +86,22 @@ class SequenceSolution(Solution):
         """
         Postprocess each solution, one at a time, in order. After a solution
         is postprocessed, the environment is saved and then jumped to the end
-        of the solution before beginning postprocessing on the next solution.
+        of the solution before postprocessing the next solution.
 
-        @param env The OpenRAVE environment
+        @param env: OpenRAVE environment
+        @return a SequenceExecutableSolution of executable solutions
         """
         executable_solutions = self._postprocess_recursive(env, self.solutions)
         return SequenceExecutableSolution(self, executable_solutions)
 
     def _postprocess_recursive(self, env, solutions):
+        """
+        Helper function to postprocess each solution in a list of solutions.
+
+        @param env: OpenRAVE environment
+        @param solutions: list of solutions to postprocess
+        @return list of executable solutions
+        """
         if not solutions:
             return []
 
@@ -89,20 +114,24 @@ class SequenceSolution(Solution):
 
 
 class SequenceAction(Action):
+    """A meta-Action that plans and executes a sequence of actions in order."""
+
     def __init__(self,
                  actions,
                  name=None,
                  precondition=None,
                  postcondition=None):
         """
-        @param actions A list of actions to be planned and executed in order
-        @param name The name of this action
+        @param actions: list of Actions
+        @param name: name of the action
+        @param precondition: Validator that validates preconditions
+        @param postcondition: Validator that validates postconditions
         """
         super(SequenceAction, self).__init__(name=name)
 
         self.actions = actions
 
-        if len(self.actions) > 0:
+        if self.actions:
             if precondition:
                 self.precondition = SequenceValidator(
                     [precondition, self.actions[0].precondition])
@@ -116,16 +145,25 @@ class SequenceAction(Action):
 
     def plan(self, env):
         """
-        Recursively plan each action in the set of actions used to construct this action.
-        After an action is planned, the environment is saved and jumped to the end of the solution
-        for the action before planning of the next action begins.
+        Recursively plan each action in the set of actions used to construct
+        this action. After an action is planned, the environment is saved and
+        jumped to the end of the solution for the action before planning of the
+        next action begins.
 
-        @param env The OpenRAVe environment
+        @param env: OpenRAVE environment
+        @return SequenceSolution of solutions to the actions
         """
         solutions = self._plan_recursive(env, self.actions)
         return SequenceSolution(self, solutions)
 
     def _plan_recursive(self, env, actions):
+        """
+        Helper function to plan each action in a list of actions.
+
+        @param env: OpenRAVE environment
+        @param actions: list of actions to plan
+        @return list of solutions
+        """
         if not actions:
             return []
 
@@ -137,9 +175,11 @@ class SequenceAction(Action):
 
     def execute(self, env, simulate):
         """
-        Plan, postprocess and execute each action, one at a time, in order.
-        @param env The OpenRAVE environment
-        @param simulate If True, simulate execution
+        Plan, postprocess, and execute each action, one at a time, in order.
+
+        @param env: OpenRAVE environment
+        @param simulate: flag to run in simulation
+        @return list of results of executing all solutions
         """
         results = []
 
